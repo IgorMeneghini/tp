93d5afacf017b0fddb1bf8184d6172a234fe8be8
IgorMeneghini <igormeneghini@outlook.com>
2023-09-29 08:43:44 -0300

    update

--- DataStructure/ExtendibleHashTable.java
+++ DataStructure/ExtendibleHashTable.java
@@ -1,7 +1,13 @@
 package DataStructure;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import Model.Film;
+import archive.Crud;
 
 public class ExtendibleHashTable<K, V> {
     private List<Bucket<Integer, Long>> directory;
@@ -17,16 +23,24 @@ public class ExtendibleHashTable<K, V> {
         }
     }
 
-    public void startHash(){
-        
+    public void startHash() throws IOException {
+        RandomAccessFile raf = new RandomAccessFile("DataBase/films.db", "r");
+        raf.seek(4);
+        while (raf.getFilePointer() < raf.length()) {
+            long address = raf.getFilePointer();
+            Film film = Crud.readFilm(raf);
+            int key = film.getId();
+            set(key, address);
+        }
     }
 
     public void set(int key, Long value) {
-        int bucketIndex = calculateBucketIndex(key); 
-        Bucket<Integer, Long> bucket = directory.get(bucketIndex); 
+        int bucketIndex = calculateBucketIndex(key);
+        Bucket<Integer, Long> bucket = directory.get(bucketIndex);
         bucket.put(key, value);
         if (bucket.size() > bucketSize) { // checking if the memory has run out
-            extendDirectory(bucketIndex); // expand the director increasing the depth and rehashing the bucket that blew out the memory
+            extendDirectory(bucketIndex); // expand the director increasing the depth and rehashing the bucket that blew
+                                          // out the memory
         }
     }
 
@@ -39,12 +53,34 @@ public class ExtendibleHashTable<K, V> {
         }
         for (Integer key : oldBucket.entries.keySet()) {
             Long value = oldBucket.get(key);
-            set(key,value);
+            set(key, value);
         }
     }
 
+    public Film get(int key) throws IOException {
+    RandomAccessFile raf = new RandomAccessFile("DataBase/films.db", "r");
+
+    int bucketIndex = calculateBucketIndex(key);
+    Bucket<Integer, Long> bucket = directory.get(bucketIndex);
+    
+    for (Map.Entry<Integer, Long> entry : bucket.entries.entrySet()) {
+        Integer tempKey = entry.getKey();
+        if (tempKey.equals(key)) {
+            raf.seek(entry.getValue());
+            Film film = Crud.readFilm(raf);
+            raf.close(); // Close the file here
+            return film;
+        }
+    }
+    
+    raf.close(); // Close the file in case the film is not found
+    return null; // Returns null if the film is not found
+}
+
+
     private int calculateBucketIndex(int key) {
-        return key % (int) Math.pow(2, directoryDepth);
+        int bucketIndex = key * (int) Math.pow(2, directoryDepth - 1);
+        return bucketIndex % directory.size();
     }
 
 }
--- app/Main.java
+++ app/Main.java
@@ -2,20 +2,25 @@ package app;
 
 import java.io.IOException;
 
+import DataStructure.ExtendibleHashTable;
 import archive.FilmParser;
-import archive.ExternalMergeSorting;
+//import archive.ExternalMergeSorting;
 
 class Main {
 
     public static void main(String[] args) throws IOException {
         FilmParser.start();
-        ExternalMergeSorting externalMergeSorting = new ExternalMergeSorting();
+        //ExternalMergeSorting externalMergeSorting = new ExternalMergeSorting();
+        ExtendibleHashTable<Integer,Long> hash = new ExtendibleHashTable<>(500);
 
         // Film film = new Film(8449, "Igor O criador", "Movie", 9999999, "R,90 min");
         // crud.update(film);
         // System.out.println(crud.delete(8449));
         // System.out.println(crud.sequencialSearch(8450));
-        externalMergeSorting.externalMergeSortSort(1000);
+        //externalMergeSorting.externalMergeSortSort(1000);
+        hash.startHash();
+        System.out.println(hash.get(8450));
+        
     }
 
 }
