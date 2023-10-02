9a0d4051d57b8fe0bcd4a8b12ef193be29891f08
IgorMeneghini <igormeneghini@outlook.com>
2023-10-01 21:01:00 -0300

    update

--- Algorithm/ExternalMergeSorting.java
+++ Algorithm/ExternalMergeSorting.java
@@ -11,25 +11,34 @@ import Model.Film;
 import archive.Crud;
 
 public class ExternalMergeSorting {
-    private RandomAccessFile raf;
-    private final String dbFilePath = "DataBase/films.db";
-    private String outputFilePath;
+    private RandomAccessFile raf; // RandomAccessFile for reading the input database
+    private final String dbFilePath = "DataBase/films.db"; // Path to the input database file
+    private String outputFilePath; // Path to the output sorted database file
 
     public ExternalMergeSorting() {
         try {
+            // Initialize the RandomAccessFile for reading the input database
             raf = new RandomAccessFile(dbFilePath, "r");
-            raf.seek(4);
-            this.outputFilePath = "DataBase/sortedFilms.db";
+            raf.seek(4); // Skip the initial data
+            this.outputFilePath = "DataBase/sortedFilms.db"; // Set the output file path
+
+            // Create and initialize a new RandomAccessFile for writing the sorted output
             RandomAccessFile raf = new RandomAccessFile(outputFilePath, "rw");
-            raf.setLength(0);
-            raf.close();
+            raf.setLength(0); // Clear the output file
+            raf.close(); // Close the output file
         } catch (Exception e) {
+            // Handle any exceptions that may occur during initialization
         }
     }
 
     public void externalMergeSortSort(int chunkSize) throws IOException {
+        // Split the input data into sorted chunks
         List<String> chunkFileNames = splitIntoSortedChunks(chunkSize);
+
+        // Merge the sorted chunks into the output file
         mergeSortedChunks2(chunkFileNames, outputFilePath);
+
+        // Clean up temporary chunk files
         cleanupTempFiles(chunkFileNames);
     }
 
@@ -39,26 +48,37 @@ public class ExternalMergeSorting {
 
         try {
             List<Film> chunk = new ArrayList<>();
+
+            // Read and process the input data in chunks
             while (raf != null && raf.getFilePointer() < raf.length()) {
                 Film film = Crud.readFilm(raf);
                 chunk.add(film);
+
                 if (chunk.size() >= chunkSize) {
+                    // Sort the chunk and save it to a temporary file
                     quickSort(chunk);
                     String chunkFileName = "DataBase/chunk" + chunkCount + ".db";
                     chunkFileNames.add(chunkFileName);
+
+                    // Create a new RandomAccessFile for the temporary chunk file
                     try (RandomAccessFile tempRaf = new RandomAccessFile(chunkFileName, "rw")) {
                         for (Film item : chunk) {
                             Crud.createInTempFiles(item, tempRaf);
                         }
                     }
+
                     chunk.clear();
                     chunkCount++;
                 }
             }
+
+            // Process the remaining data if any
             if (!chunk.isEmpty()) {
                 quickSort(chunk);
                 String chunkFileName = "DataBase/chunk" + chunkCount + ".db";
                 chunkFileNames.add(chunkFileName);
+
+                // Create a new RandomAccessFile for the remaining chunk data
                 try (RandomAccessFile tempRaf = new RandomAccessFile(chunkFileName, "rw")) {
                     for (Film item : chunk) {
                         Crud.createInTempFiles(item, tempRaf);
@@ -74,26 +94,42 @@ public class ExternalMergeSorting {
         return chunkFileNames;
     }
 
+    /**
+     * Merge sorted chunks of data into a single output file.
+     * 
+     * @param chunkFileNames - The names of the chunk files to be merged.
+     * @param outputFile     - The name of the output file.
+     */
     private void mergeSortedChunks2(List<String> chunkFileNames, String outputFile) throws IOException {
-        Crud crud = new Crud(true);
-        List<RandomAccessFile> tempRaf = createRandomAccessFiles(chunkFileNames);
-        List<Film> queue = startQueue(tempRaf);
+        Crud crud = new Crud(true); // Initialize a CRUD instance for writing
+        List<RandomAccessFile> tempRaf = createRandomAccessFiles(chunkFileNames); // Create RandomAccessFiles for
+                                                                                  // reading chunks
+        List<Film> queue = startQueue(tempRaf); // Initialize a queue for merging
 
+        // Continue merging until there's data left in the queue
         while (notEmpty(queue, chunkFileNames.size())) {
-            Film film = lower(queue);
+            Film film = lower(queue); // Find the lowest film from the queue
             if (film != null) {
-                int index = queue.indexOf(film);
+                int index = queue.indexOf(film); // Get the index of the lowest film in the queue
                 if (tempRaf.get(index).getFilePointer() < tempRaf.get(index).length()) {
+                    // Read the next film from the corresponding chunk if available
                     queue.set(index, Crud.readFilm(tempRaf.get(index)));
                 } else {
-                    queue.set(index, null);
+                    queue.set(index, null); // Mark the queue as empty for this chunk
                 }
-                crud.create(film, outputFile);
+                crud.create(film, outputFile); // Write the lowest film to the output file
             }
         }
-        crud.readAll(outputFile);
+        crud.readAll(outputFile); // Read and display all records from the sorted output file
     }
 
+    /**
+     * Check if there's any data left in the queue.
+     * 
+     * @param queue - The queue of films from different chunks.
+     * @param size  - The total number of chunks.
+     * @return True if there's data left in the queue; otherwise, false.
+     */
     boolean notEmpty(List<Film> queue, int size) {
         int cont = 0;
         for (Film film : queue) {
@@ -102,95 +138,146 @@ public class ExternalMergeSorting {
             }
         }
         if (cont == size) {
-            return false;
+            return false; // All chunks are empty
         }
-        return true;
+        return true; // At least one chunk has data remaining
     }
 
+    /**
+     * Find the lowest film based on the title from the queue.
+     * 
+     * @param queue - The queue of films from different chunks.
+     * @return The lowest film in the queue.
+     */
     private Film lower(List<Film> queue) {
-        Film lower = queue.get(0);
+        Film lower = queue.get(0); // Initialize with the first film in the queue
         if (lower != null) {
             for (Film film : queue) {
                 if (film != null && lower.getTitle().compareTo(film.getTitle()) > 0) {
-                    lower = film;
+                    lower = film; // Update 'lower' if a lower film is found
                 }
             }
         } else {
             for (int i = 1; i < queue.size(); i++) {
                 lower = queue.get(i);
                 if (lower != null) {
-                    break;
+                    break; // Find the first non-null film
                 }
             }
             if (lower != null) {
                 for (Film film : queue) {
                     if (film != null && lower.getTitle().compareTo(film.getTitle()) > 0) {
-                        lower = film;
+                        lower = film; // Update 'lower' if a lower film is found
                     }
                 }
             }
         }
-        return lower;
+        return lower; // Return the lowest film in the queue
     }
 
+    /**
+     * Initialize a queue of films by reading the first film from each
+     * RandomAccessFile.
+     * 
+     * @param tempRaf - List of RandomAccessFiles representing chunk files.
+     * @return List of films in the initial queue.
+     */
     private List<Film> startQueue(List<RandomAccessFile> tempRaf) throws IOException {
         List<Film> queue = new ArrayList<>();
         for (RandomAccessFile randomAccessFile : tempRaf) {
-            queue.add(Crud.readFilm(randomAccessFile));
+            queue.add(Crud.readFilm(randomAccessFile)); // Read the first film from each chunk
         }
         return queue;
     }
 
+    /**
+     * Create a list of RandomAccessFiles for reading chunk files.
+     * 
+     * @param chunkFileNames - List of chunk file names.
+     * @return List of RandomAccessFiles for reading the chunk files.
+     */
     private List<RandomAccessFile> createRandomAccessFiles(List<String> chunkFileNames) throws FileNotFoundException {
         List<RandomAccessFile> tempRaf = new ArrayList<>();
         for (String chunkFileName : chunkFileNames) {
-            tempRaf.add(new RandomAccessFile(chunkFileName, "rw"));
+            tempRaf.add(new RandomAccessFile(chunkFileName, "rw")); // Create RandomAccessFiles for reading the chunks
         }
         return tempRaf;
     }
 
+    /**
+     * Clean up temporary chunk files by deleting them.
+     * 
+     * @param chunkFileNames - List of temporary chunk file names.
+     */
     private void cleanupTempFiles(List<String> chunkFileNames) {
         for (String fileName : chunkFileNames) {
             File file = new File(fileName);
             if (file.exists()) {
-                file.delete();
+                file.delete(); // Delete the temporary chunk files
             }
         }
     }
 
+    /**
+     * Perform a quicksort on the list of films.
+     * 
+     * @param arr - List of films to be sorted.
+     */
     public void quickSort(List<Film> arr) {
         if (arr == null || arr.size() <= 1) {
-            return; // No need to sort
+            return; // No need to sort if the list is empty or has only one element
         }
         quickSort(arr, 0, arr.size() - 1);
     }
 
+    /**
+     * Recursive function to perform quicksort on a portion of the list.
+     * 
+     * @param arr  - List of films to be sorted.
+     * @param low  - Starting index of the portion to be sorted.
+     * @param high - Ending index of the portion to be sorted.
+     */
     private void quickSort(List<Film> arr, int low, int high) {
         if (low < high) {
             int pivotIndex = partition(arr, low, high);
-            quickSort(arr, low, pivotIndex - 1);
-            quickSort(arr, pivotIndex + 1, high);
+            quickSort(arr, low, pivotIndex - 1); // Recursively sort the left partition
+            quickSort(arr, pivotIndex + 1, high); // Recursively sort the right partition
         }
     }
 
+    /**
+     * Partitioning step of the quicksort algorithm.
+     * 
+     * @param arr  - List of films to be sorted.
+     * @param low  - Starting index of the partition.
+     * @param high - Ending index of the partition.
+     * @return Index of the pivot element.
+     */
     private int partition(List<Film> arr, int low, int high) {
-        Film pivot = arr.get(high);
+        Film pivot = arr.get(high); // Choose the pivot element (last element in the partition)
         int i = low - 1;
 
         for (int j = low; j < high; j++) {
             if (arr.get(j).getTitle().compareTo(pivot.getTitle()) < 0) {
                 i++;
-                swap(arr, i, j);
+                swap(arr, i, j); // Swap elements if they are out of order
             }
         }
 
-        swap(arr, i + 1, high);
+        swap(arr, i + 1, high); // Swap the pivot element into its correct position
         return i + 1;
     }
 
+    /**
+     * Helper method to swap two elements in a list.
+     * 
+     * @param arr - List of films.
+     * @param i   - Index of the first element.
+     * @param j   - Index of the second element.
+     */
     private void swap(List<Film> arr, int i, int j) {
         Film temp = arr.get(i);
-        arr.set(i, arr.get(j));
+        arr.set(i, arr.get(j)); // Swap elements at index i and j
         arr.set(j, temp);
     }
 }
--- app/Main.java
+++ app/Main.java
@@ -12,103 +12,116 @@ import Algorithm.ExternalMergeSorting;
 import Model.Film;
 
 class Main {
-
-    private static final String dbFilePath = "DataBase\\films.db";
+    private static final String dbFilePath = "DataBase\\films.db"; // Path to the database file
 
     public static void main(String[] args) throws IOException {
-        FilmParser.start();
-        int option = 0;
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.println("Selecione:\n" + "1- CRUD\n" + "2- Ordenação Externa\n" + "3- Hash\n"
-                    + "4- Lista Invertida\n" + "5- Árvore B\n6- Sair");
-            option = Integer.parseInt(sc.nextLine());
+        FilmParser.start(); // Start parsing data
 
-            switch (option) {
-                case 1:
-                    Crud crud = new Crud(true); // Supondo que você tenha uma classe Crud
-                    while (true) {
-                        System.out.println("O que quer fazer?:\n" + "1- Create\n" + "2- Read\n" + "3- Update\n"
-                                + "4- Delete\n" + "5- Sair");
-                        option = Integer.parseInt(sc.nextLine());
+        int option = 0; // Initialize the option variable
+        try (Scanner sc = new Scanner(System.in)) {
+            while (true) { // Start an infinite loop for the main menu
+                System.out.println("Selecione:\n" +
+                        "1- CRUD\n" +
+                        "2- Ordenação Externa\n" +
+                        "3- Hash\n" +
+                        "4- Lista Invertida\n" +
+                        "5- Árvore B\n" +
+                        "6- Sair");
 
-                        switch (option) {
-                            case 1:
+                option = Integer.parseInt(sc.nextLine()); // Read user input as an integer
+
+                switch (option) {
+                    case 1:
+                        Crud crud = new Crud(true); // Create a CRUD instance
+                        while (true) { // Start an inner loop for CRUD operations
+                            System.out.println("O que quer fazer?:\n" +
+                                    "1- Create\n" +
+                                    "2- Read\n" +
+                                    "3- Update\n" +
+                                    "4- Delete\n" +
+                                    "5- Sair");
+
+                            option = Integer.parseInt(sc.nextLine()); // Read user input for CRUD operation
+
+                            switch (option) {
+                                case 1:
+                                    Film film = new Film(8555, "Igor O criador", "Movie", 9999999, "R,90 min");
+                                    crud.create(film, dbFilePath);
+                                    break;
+                                case 2:
+                                    crud.readAll(dbFilePath);
+                                    break;
+                                case 3:
+                                    film = new Film(1, "Igor O criador", "Movie", 9999999, "R,90 min");
+                                    crud.update(film);
+                                    break;
+                                case 4:
+                                    crud.delete(1);
+                                    break;
+                                case 5:
+                                    System.out.println("Saindo do CRUD.");
+                                    break;
+                                default:
+                                    System.out.println("Opção inválida. Por favor, selecione uma opção válida.");
+                                    break;
+                            }
+                            if (option == 5) {
+                                break; // Exit the inner CRUD loop
+                            }
+                        }
+                        break;
+                    case 2:
+                        ExternalMergeSorting externalMergeSorting = new ExternalMergeSorting();
+                        externalMergeSorting.externalMergeSortSort(1000);
+                        FilmParser.start();
+                        break;
+                    case 3:
+                        ExtendibleHashTable<Integer, Long> hash = new ExtendibleHashTable<>(500);
+                        hash.startHash();
+                        System.out.println("Selecione um ID de busca: ");
+                        int id = Integer.parseInt(sc.nextLine());
+                        System.out.println(hash.get(id));
+                        break;
+                    case 4:
+                        option = 1;
+                        while (true) {
+                            System.out.println("1- Lista 1\n2- Lista 2\n3- sair");
+                            option = Integer.parseInt(sc.nextLine());
+                            if (option == 1) {
+                                InvertedList invertedList1 = new InvertedList();
                                 Film film = new Film(8555, "Igor O criador", "Movie", 9999999, "R,90 min");
-                                crud.create(film, dbFilePath);
+                                Film film2 = new Film(8556, "luis O criador", "Movie", 9999999, "R,70 min");
+                                invertedList1.startList();
+                                invertedList1.insert(film);
+                                System.out.println(invertedList1.search(film));
+                                System.out.println(invertedList1.search(film2));
+                            } else if (option == 2) {
+                                InvertedList2 invertedList2 = new InvertedList2();
+                                Film film = new Film(8555, "Igor O criador", "Movie", 9999999, "R,50 min");
+                                Film film2 = new Film(8556, "luis O criador", "Movie", 9999999, "R,70 min");
+                                invertedList2.startList();
+                                invertedList2.insert(film);
+                                System.out.println(invertedList2.search(film));
+                                System.out.println(invertedList2.search(film2));
+                            } else {
                                 break;
-                            case 2:
-                                crud.readAll(dbFilePath);
-                                break;
-                            case 3:
-                                film = new Film(1, "Igor O criador", "Movie", 9999999, "R,90 min");
-                                crud.update(film);
-                                break;
-                            case 4:
-                                crud.delete(1);
-                                break;
-                            case 5:
-                                System.out.println("Saindo do CRUD.");
-                                break;
-
-                            default:
-                                System.out.println("Opção inválida. Por favor, selecione uma opção válida.");
-                                break;
+                            }
                         }
-                        if (option == 5) {
-                            break;
-                        }
-                    }
-                    break;
-                case 2:
-                    ExternalMergeSorting externalMergeSorting = new ExternalMergeSorting();
-                    externalMergeSorting.externalMergeSortSort(1000);
-                    FilmParser.start();
-                    break;
-
-                case 3:
-                    ExtendibleHashTable<Integer, Long> hash = new ExtendibleHashTable<>(500);
-                    hash.startHash();
-                    System.out.println("Selecione um ID de busca: ");
-                    int id = Integer.parseInt(sc.nextLine());
-                    System.out.println(hash.get(id));
-                    break;
-                case 4:
-                    option = 1;
-                    while (true) {
-                        System.out.println("1- Lista 1\n2- Lista 2\n3- sair");
-                        option = Integer.parseInt(sc.nextLine());
-                        if (option == 1) {
-                            InvertedList invertedList1 = new InvertedList();
-                            Film film = new Film(8555, "Igor O criador", "Movie", 9999999, "R,90 min");
-                            Film film2 = new Film(8556, "luis O criador", "Movie", 9999999, "R,70 min");
-                            invertedList1.startList();
-                            invertedList1.insert(film);
-                            System.out.println(invertedList1.search(film));
-                            System.out.println(invertedList1.search(film2));
-                        } else if (option == 2) {
-                            InvertedList2 invertedList2 = new InvertedList2();
-                            Film film = new Film(8555, "Igor O criador", "Movie", 9999999, "R,50 min");
-                            Film film2 = new Film(8556, "luis O criador", "Movie", 9999999, "R,70 min");
-                            invertedList2.startList();
-                            invertedList2.insert(film);
-                            System.out.println(invertedList2.search(film));
-                            System.out.println(invertedList2.search(film2));
-                        } else {
-                            break;
-                        }
-                    }
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    System.out.println("Saindo");
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Opção inválida. Por favor, selecione uma opção válida.");
-                    break;
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        System.out.println("Saindo");
+                        System.exit(0); // Exit the program
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Por favor, selecione uma opção válida.");
+                        break;
+                }
             }
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
